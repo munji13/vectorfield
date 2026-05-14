a2d917d11128096de3ee305145bf8ab174114587
Kkiruk <140838697+munji13@users.noreply.github.com>
2026-05-14 16:51:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+x, y = np.meshgrid(np.linspace(0, 400, 15), np.linspace(0, 400, 15))
+P = 0.1*x*(1 - 0.01*x + 0.005*y)
+Q = 0.03*y*(1 + 0.04*x - 0.01*y)
+M = np.sqrt(P**2 + Q**2)
+
+plt.quiver(x, y, P/M, Q/M)
+plt.show()
